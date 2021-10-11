285c5579d3ea393cfafd70f921ecdb6b1470a1b1
Saengsorn-Wongpriew <saengsorn.w@kkumail.com>
2021-10-11 11:26:12 +0700

    add lab4

---
+++ lab4/New Unity Project (1)/Assets/ExampleScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0.0f;
+    public float distance = 0.0f;
+    public float time = 0.0f;
+    void Start()
+    {
+        
+    }
+
+    void SpeedCheck()
+    {
+        speed = distance / time;
+        if (speed > 70 || speed < 40)
+        {
+            print("You are breaking the law!");
+        }
+        print("you are traveling at " + speed + "MPH");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpeedCheck();
+        }
+    }
+}
---
+++ lab4/New Unity Project (1)/Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0.0f;
+    public float distance = 0.0f;
+    public float time = 0.0f;
+    void Start()
+    {
+        speed = distance / time;
+        print("you are traveling at " + speed + "MPH");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
